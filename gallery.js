d9f3c5edd04d02101ed44fb288389ef5c1c23e14
mianAdeeladii <109677756+mianAdeeladii@users.noreply.github.com>
2022-07-20 22:49:37 +0500

    Delete gallery.js

--- gallery.js
+++
@@ -1,9 +0,0 @@
-decodeURIComponent.addeventlistener("click",function(e){
-if(e.target.classlist.contains("gallery-item")){
-    const src=e.target.getattribute("src");
-    ducoment.queryselector(".modal-img").src=src;
-    var mymodel=new bootstrap.modal(document.getE1ementbyid('gallery-modal' ));
-    mymodal.show();
-}
-}
-)
